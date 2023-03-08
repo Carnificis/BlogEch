65fa1f2f9ba5bc1a8924b8e47816c45657a076f2
Jack Kelly <Carnificis@users.noreply.github.com>
2023-03-07 23:31:21 -0600

    seeds

---
+++ controllers/commentRoutes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const { Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const comments = await Comment.findAll();
+    res.json(comments);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const comment = await Comment.create(req.body);
+    res.json(comment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const comment = await Comment.findByPk(req.params.id);
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+    const updatedComment = await comment.update(req.body);
+    res.json(updatedComment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const comment = await Comment.findByPk(req.params.id);
+    if (!comment) {
+      return res.status(404).json({ message: 'Comment not found' });
+    }
+    await comment.destroy();
+    res.json({ message: 'Comment deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,10 @@ const { User, Project } = require('../models');
 
 router.get('/', (req, res)=> {
 
-    res.render('homepage')
+    //res.render('homepage')
+    res.render('homepage', {
+        logged_in: req.session.logged_in
+    })
 
 });
 
