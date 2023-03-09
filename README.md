65a425eeca23f856ea7b2e32773aeb27febed8a8
Jack Kelly <Carnificis@users.noreply.github.com>
2023-03-08 22:39:27 -0600

    postData and userData

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # BlogEch
+
+## Description
+A tech blog that provides a list of posts that have been published on the blog
+
+### Author
+Jack Kelly
+
+### Live Link
+()
+
+#### Repo
+()
---
+++ seeds/userData.js
@@ -0,0 +1,63 @@
+[
+
+    {
+        "username:" "John Jones"
+        "email:"    "johnjones@gmail.com"
+        "password:" "qwerty1234567890!@#$%^&*"
+    }
+
+    {
+        "username:" "Bob Smith"
+        "email:"    "bobsmith@gmail.com"
+        "password:" "test12"
+    }
+
+    {
+        "username:" "Chris Pine"
+        "email:"    "chrispine@gmail.com"
+        "password:" "jsihudfjdbsckjd"
+    }
+
+    {
+        "username:" "Andrew Adamson"
+        "email:"    "andrewadamson@gmail.com"
+        "password:" "iocwdohuvjekdcs"
+    }
+
+    {
+        "username:" "Derek Harris"
+        "email:"    "derekharris@gmail.com"
+        "password:" "xpowocedklncsakl"
+    }
+
+    {
+        "username:" "Freddie Mercury"
+        "email:"    "freddiemercury@gmail.com"
+        "password:" "pcopwkecmcm"
+    }
+
+    {
+        "username:" "George Harrison"
+        "email:"    "georgeharrison@gmail.com"
+        "password:" "ncwojvjnvdnsdncs"
+    }
+
+    {
+        "username:" "Henry Hudson"
+        "email:"    "henryhudson@gmail.com"
+        "password:" "gbvurvffjkd"
+    }
+
+    {
+        "username:" "Ian Mansfield"
+        "email:"    "ianmansfield@gmail.com"
+        "password:" "akskdjdnc"
+    }
+
+    {
+        "username:" "Larry Lee"
+        "email:"    "larrylee@gmail.com"
+        "password:" "jdfkdnc"
+    }
+
+]
