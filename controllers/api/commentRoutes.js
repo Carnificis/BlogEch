53a4eefa42c93540eba32515fa0189b599ce5695
Jack Kelly <Carnificis@users.noreply.github.com>
2023-03-09 19:31:24 -0600

    Pushing to Heroku

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Comment } = require('../models');
+const { Comment } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,34 +1,94 @@
-const router = require('express').Router();
-const { User, Project } = require('../models');
+const router = require("express").Router();
+const { User, Post } = require("../models");
+const withAuth = require("../utils/auth");
+// router.get("/", (req, res) => {
+//   //res.render('homepage')
+//   res.render("homepage", {
+//     logged_in: req.session.logged_in,
+//   });
+// });
 
-router.get('/', (req, res)=> {
-
-    //res.render('homepage')
-    res.render('homepage', {
-        logged_in: req.session.logged_in
-    })
-
-});
-
-
-router.get('/login', (req, res)=> {
-
-    //res.render('homepage')
-    res.render('signup', {
-        logged_in: req.session.logged_in
-    })
-
-});
-
-router.get('/logout', (req, res) => {
-    if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.redirect('/');
-    }
+router.get("/login", (req, res) => {
+  //res.render('homepage')
+  res.render("signup", {
+    logged_in: req.session.logged_in,
   });
+});
 
+router.get("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    // Get all Posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const Posts = postData.map((PostData) => PostData.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      Posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/Post/:id", async (req, res) => {
+  try {
+    const PostData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const Post = PostData.get({ plain: true });
+
+    res.render("Post", {
+      ...Post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
