25d3036e38e4a6a47a0ecead003c1a729e026a10
Jack Kelly <Carnificis@users.noreply.github.com>
2023-04-02 14:59:47 -0500

    routes

--- models/index.js
+++ models/index.js
@@ -2,9 +2,9 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-Post.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Post.hasMany(Comment, {
